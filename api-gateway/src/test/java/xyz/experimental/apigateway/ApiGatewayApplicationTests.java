df1d552fc53cf14dd2649d01685b949469032a91
Sumedh Kumar <sumedhkumar96@gmail.com>
2021-06-14 19:28:02 +0530

    Added API gateway.

---
+++ api-gateway/src/test/java/xyz/experimental/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.experimental.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
