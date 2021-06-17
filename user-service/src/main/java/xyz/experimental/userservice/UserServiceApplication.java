1c7e79665f23d32249655ca6f2370f244809a4d2
Sumedh Kumar <sumedhkumar96@gmail.com>
2021-06-17 21:40:19 +0530

    Modified properties.

--- user-service/src/main/java/xyz/experimental/userservice/UserServiceApplication.java
+++ user-service/src/main/java/xyz/experimental/userservice/UserServiceApplication.java
@@ -6,9 +6,11 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = { "xyz.experimental.userservice", "xyz.experimental.commonutil", "xyz.experimental.authutil" })
 public class UserServiceApplication {
 
